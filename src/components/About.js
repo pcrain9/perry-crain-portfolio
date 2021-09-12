bd7e93429befdd05b508d6ad43f0417dd848c588
Perry Crain <pcrain9@gmail.com>
2021-09-12 14:04:35 -0500

    foo

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
     return (
         <div className={classes.container}>
             <h1>About me</h1>
-            <p>lorem ipsum</p>
+            <p>From understanding foundations of computing using C and Java to dedicating myself to the inner-workings of front- and back-end web, I’ve mastered React.js (functional and class-based components). I learned core features of CSS, HTML, and JavaScript, at which I am now adept. My back-end learning involved diving into core features of Node.js and the Express.js library.  I a learning more and more about web development every day and my passion for the field has never wavered!</p>
             <NavLink to="/about">click here for resume</NavLink>
         </div>
     )
