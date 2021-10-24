7b58e31fcceae3d65579ebec15d3f63b9b19fb73
Perry <80589286+pcrain9@users.noreply.github.com>
2021-10-24 13:08:21 -0500

    making changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import { Typography } from '@mui/material';
+import { Link } from '@mui/material';
 
 import classes from './About.module.css';
 
 function About() {
     return (
         <div className={classes.container}>
-            <h1>About me</h1>
-            <p>From understanding foundations of computing using C and Java to dedicating myself to the inner-workings of front- and back-end web, I’ve mastered React.js (functional and class-based components). I learned core features of CSS, HTML, and JavaScript, at which I am now adept. My back-end learning involved diving into core features of Node.js and the Express.js library.  I a learning more and more about web development every day and my passion for the field has never wavered!</p>
-            <a className={classes.link} to='https://drive.google.com/file/d/1QeEB7l3W1PkH7rD4P_b5yCDnXslZVBaT/view?usp=sharing'>Check out my resume here: </a>
+            <Typography variant='h3'>About me</Typography>
+            <Typography variant='p'>From understanding foundations of computing using C and Java to dedicating myself to the inner-workings of front- and back-end web, I’ve mastered React.js (functional and class-based components). I learned core features of CSS, HTML, and JavaScript, at which I am now adept. My back-end learning involved diving into core features of Node.js and the Express.js library.  I a learning more and more about web development every day and my passion for the field has never wavered!</Typography>
+            <br />
+            <Link underline='none' color='primary' variant='body2' href='https://drive.google.com/file/d/1QeEB7l3W1PkH7rD4P_b5yCDnXslZVBaT/view?usp=sharing'>Check out my resume here </Link>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import { Typography } from "@mui/material";
 import { AppBar } from "@mui/material";
 import { Link } from "@mui/material";
 import { Menu } from "@mui/material";
+import { Button } from "@mui/material";
 import { MenuItem } from "@mui/material";
 import ToolBar from "@mui/material/Toolbar";
 
 import classes from "./Header.module.css";
 
 function Header() {
+  const [anchorEl, setAnchorE1] = useState(null);
+  const open = Boolean(anchorEl);
+
+  function handleClick(event) {
+    setAnchorE1(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorE1(null);
+  }
+
   return (
-    <AppBar position="relative">
-      <ToolBar> 
-      <NavLink className={classes.nav_item} to="/">
-        Home
-      </NavLink>
-      <NavLink to="/portfolio" className={classes.nav_item}>
-        Portfolio
-      </NavLink>
-      
-        <NavLink to="/about" className={classes.nav_item}>
-          About
-        </NavLink>
-        <Menu className={classes.dropdowncontent}>
-          <MenuItem>
-            <NavLink to="/about" className={classes.link}>
-              about
-            </NavLink>
-          </MenuItem>
-          <MenuItem>
-            <Link
-              href="https://drive.google.com/file/d/1QeEB7l3W1PkH7rD4P_b5yCDnXslZVBaT/view?usp=sharing"
-              variant="body2"
-            >
-              resume(link to google docs)
-            </Link>
-          </MenuItem>
-          <MenuItem>
-            <Link href="https://github.com/pcrain9" variant="body2">
-              github
-            </Link>
-          </MenuItem>
-        </Menu>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <ToolBar>
+          <NavLink className={classes.nav_item} to="/">
+            Home
+          </NavLink>
+          <NavLink to="/portfolio" className={classes.nav_item}>
+            Portfolio
+          </NavLink>
+          <Button
+            id="basic-button"
+            aria-controls="basic-menu"
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            position='inherit'
+            onClick={handleClick}
+            sx={{
+              margin:'auto'
+            }}
+          >
+            About
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <Typography variant="p">About</Typography>
+            <MenuItem onClick={handleClose}>
+              <NavLink to="/about" className={classes.link}>
+                about
+              </NavLink>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link
+                href="https://drive.google.com/file/d/1QeEB7l3W1PkH7rD4P_b5yCDnXslZVBaT/view?usp=sharing"
+                variant="body2"
+              >
+                resume(link to google docs)
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link href="https://github.com/pcrain9" variant="body2">
+                github
+              </Link>
+            </MenuItem>
+          </Menu>
         </ToolBar>
-      <Link href="mailto:pcrain9@gmail.com">
-        Contact
-      </Link>
-      
-    </AppBar>
+        <Link href="mailto:pcrain9@gmail.com">Contact</Link>
+      </AppBar>
+    </>
   );
 }
 
